a327f2702b7ce7c7eaf5ff6f90beee160a34bf67
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2024-09-14 23:20:04 +0200

    apps(api) : add ingredients function

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,45 @@
 import express, { Request, Response } from 'express';
 
-const server = express();
-const port: number = 8000;
+const app = express();
+const port = process.env.PORT || 8000;
 
-server.get('/hello', (req: Request, res: Response): void => {
-    res.send("Hello World");
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, TypeScript Express!');
 });
 
-server.listen(port, (): void => {
-    console.log(`Listening on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+interface Ingredient {
+    name: string;
+    quantity?: number
+}
+  
+const ingredients: Ingredient[] = [
+    { name: 'Argent', quantity: 12 },
+    { name: 'Bave de lama' },
+    { name: 'Épine de hérisson' },
+    { name: 'Plume de griffon' },
+    { name: 'Hélium liquide' },
+    { name: 'Poil de yéti' },
+    { name: 'Or' },
+    { name: 'Azote liquide' },
+    { name: 'Queue d\'écureuil' },
+    { name: 'Crin de licorne' },
+    { name: 'Jus de Horglup' },
+    { name: 'Noix de coco' },
+    { name: 'Yttrium' },
+    { name: 'Mandragore' }
+];
+
+// const ingredientsMiddleware = (req: Request, res: Response) => {
+//     res.json({ ingredients });
+// };
+  
+  
+// app.use('/ingredients', ingredientsMiddleware);
+
+app.get('/ingredients', (req: Request, res: Response) => {
+    res.json(ingredients);
 });
